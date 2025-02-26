61b605a0760e2f618d7473f883450d6a4a769dbc
Zachary Hamm <zjhamm304@gmail.com>
2025-02-26 14:53:37 -0500

    add:1 retry

--- cypress.config.ts
+++ cypress.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
   e2e: {
     specPattern: "cypress/e2e/**/*.cy.{js,ts}",
     video: false,
+    retries: 1,
     setupNodeEvents(on) {
       addCypressLoadBalancerPlugin(on);
     }
