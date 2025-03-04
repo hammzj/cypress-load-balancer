7f91f6b4148d382c95387aa4af4d563d943bb024
Zachary Hamm <zjhamm304@gmail.com>
2025-03-04 13:43:28 -0500

    add:export yargs client

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
 import { performLoadBalancing, addCypressLoadBalancerPlugin, mergeLoadBalancingMapFiles } from "./src";
+import { default as cli } from "./src/cli";
 
-export { performLoadBalancing, addCypressLoadBalancerPlugin, mergeLoadBalancingMapFiles };
+export { performLoadBalancing, addCypressLoadBalancerPlugin, mergeLoadBalancingMapFiles, cli };
